01249c5e80ccd6b72e037456156be3ee00322188
maksym5 <maksym.kupriichuk@gmail.com>
2017-08-07 17:00:25 +0300

    add gulp-wait

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -8,6 +8,7 @@ var notify       = require("gulp-notify");
 var cleanCSS     = require('gulp-clean-css');
 var sourcemaps   = require('gulp-sourcemaps');
 var rename       = require('gulp-rename');
+var wait         = require('gulp-wait');
 var postcss      = require('gulp-postcss');
 var csco         = require('postcss-csso');
 var browserSync  = require('browser-sync');
@@ -30,6 +31,7 @@ gulp.task('sass', function() {
 	return gulp.src(config.src.sass + "/**/*.{sass,scss}")
 	.pipe(sourcemaps.init())
 	.pipe(sass({outputStyle: 'expand'}))
+	.pipe(wait(70)) //optional
 	.on('error', config.errorHandler)
 	.pipe(rename({suffix: '.min', prefix : ''}))
 	.pipe(postcss(processors))
