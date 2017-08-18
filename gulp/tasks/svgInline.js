745493c9da93f5bb4190643f800f8d9b2fc88ca0
maksym5 <maksymdd@gmail.com>
2017-08-19 00:45:04 +0300

    add gulp-color

--- gulp/tasks/svgInline.js
+++ gulp/tasks/svgInline.js
@@ -5,12 +5,14 @@ var cheerio = require('gulp-cheerio');
 var runSequence = require('run-sequence');
 var print = require('gulp-print');
 var newer = require('gulp-newer');
+var color = require('gulp-color');
 
 gulp.task('svgRemoveAttr', function () {
 	return gulp.src(config.src.svgInline + "/fill_removed/*.svg")
 		.pipe(newer(config.src.svgInline))
 		.pipe(print(function(filename) {
-			return "NEW: " + filename;
+			var colors1 = color("NEW: " + filename.slice(36), "GREEN");
+			return colors1;
 		}))
 		.pipe(cheerio({
 			run: function ($, file) {
@@ -37,7 +39,8 @@ gulp.task('svgDefault', function () {
 	return gulp.src(config.src.svgInline + "/default/*.svg")
 	.pipe(newer(config.src.svgInline))
 	.pipe(print(function(filename) {
-		return "NEW: " + filename;
+		var colors2 = color("NEW: " + filename.slice(31), "GREEN");
+		return colors2;
 	}))
 	.pipe(cheerio({
 			run: function ($, file) {
