4ab84ead2225a90855ebcd98363ed428e5f59141
maksym5 <maksymdd@gmail.com>
2017-08-30 14:19:09 +0300

    Update lint.js

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -1,32 +1,38 @@
 var gulp        = require('gulp');
 var config      = require('../config');
 var htmlhint    = require('gulp-htmlhint');
-var eslint      = require('eslint');
+var eslint      = require('gulp-eslint');
 var sassLint    = require('gulp-sass-lint');
 
 gulp.task('lint:js', function() {
-	// return gulp.src([config.js + '*.js', '!node_modules/**'])
-	// 	.pipe(eslint())
-	// 	.pipe(eslint.format())
-	// 	.pipe(eslint.failAfterError());
+  return gulp.src([config.js + '/**/*.js', '!node_modules/**'])
+    .pipe(eslint({
+      fix: true,
+      rules: {
+        'no-undef': 0
+      },
+      globals: ['$']
+}))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 
 gulp.task('lint:html', function() {
-	return gulp.src(config.root + "/*.html")
-		.pipe(htmlhint('.htmlhintrc'))
-		.pipe(htmlhint.failReporter());
+  return gulp.src(config.root + "/*.html")
+    .pipe(htmlhint('.htmlhintrc'))
+    .pipe(htmlhint.failReporter());
 });
 
 gulp.task('lint:sass', function() {
-	return gulp.src(config.sass + '/**/*.s+(a|c)ss')
-		.pipe(sassLint())
-		.pipe(sassLint.format())
-		.pipe(sassLint.failOnError())
+  return gulp.src(config.sass + '/**/*.s+(a|c)ss')
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
 });
 
 gulp.task('lint', [
-		// 'lint:js',
-		'lint:sass',
-		'lint:html'
+  'lint:js',
+  'lint:sass',
+  'lint:html'
 ]);
